a8d0aac22073a6b920a1e0ac2c3119bc1cf26ca8
Alex Veenendaal <alex@opendoorgonorth.com>
2024-11-27 11:31:43 +0000

    initial chicken

---
+++ src/components/Debug/Debug.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, View } from 'react-native';
+
+import { makeMutable } from 'react-native-reanimated';
+
+import { ReText } from './ReText';
+
+const debugMsg = makeMutable<string>('Hello');
+const debugMsg2 = makeMutable<string>('World');
+const debugMsg3 = makeMutable<string>('!');
+const debugMsg4 = makeMutable<string>('4');
+const debugMsg5 = makeMutable<string>('5');
+
+export const setDebugMsg1 = (msg: string) => {
+  'worklet';
+  debugMsg.value = msg;
+};
+export const setDebugMsg2 = (msg: string) => {
+  'worklet';
+  debugMsg2.value = msg;
+};
+export const setDebugMsg3 = (msg: string) => {
+  'worklet';
+  debugMsg3.value = msg;
+};
+export const setDebugMsg4 = (msg: string) => {
+  'worklet';
+  debugMsg4.value = msg;
+};
+export const setDebugMsg5 = (msg: string) => {
+  'worklet';
+  debugMsg5.value = msg;
+};
+
+export const Debug = () => {
+  return (
+    <View style={styles.container}>
+      <ReText style={styles.debugText} text={debugMsg} />
+      <ReText style={styles.debugText} text={debugMsg2} />
+      <ReText style={styles.debugText} text={debugMsg3} />
+      <ReText style={styles.debugText} text={debugMsg4} />
+      <ReText style={styles.debugText} text={debugMsg5} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 46,
+    width: '80%'
+  },
+  debugText: {
+    color: '#fff'
+  }
+});
