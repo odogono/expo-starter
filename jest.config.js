499c3bb0c2446c75755ff8781717d814c68fe00a
Alex Veenendaal <alex@opendoorgonorth.com>
2025-11-25 11:27:39 +0000

    chore: expo 54

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const jestConfig = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@shopify/react-native-skia/.*|@sentry/react-native|native-base|react-native-svg)'
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)'
   ]
 };
 
